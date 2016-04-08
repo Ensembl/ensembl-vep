39ffe991cb051bc19825db1cb221818d9ad05861
William McLaren <wm2@ebi.ac.uk>
2016-04-08 14:33:31 +0100

    add travis framework

---
+++ travisci/harness.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-6-924:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/modules:$PWD/ensembl-io/modules:$PWD/ensembl-funcgen/modules
+
+export PATH=$PATH:$PWD/htslib
+
+echo "Running test suite"
+echo "Using $PERL5LIB"
+if [ "$COVERALLS" = 'true' ]; then
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test,+ignore,ensembl,+ignore,ensembl-io,+ignore,ensembl-funcgen' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t $SKIP_TESTS
+else
+  perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t $SKIP_TESTS
+fi
+
+rt=$?
+if [ $rt -eq 0 ]; then
+  if [ "$COVERALLS" = 'true' ]; then
+    echo "Running Devel::Cover coveralls report"
+    cover --nosummary -report coveralls
+  fi
+  exit $?
+else
+  exit $rt
+fi
