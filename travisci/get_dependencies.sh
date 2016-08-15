9ec9e2b31e90f70d02d68d8dfa7133b3d152a9bc
William McLaren <wm2@ebi.ac.uk>
2016-08-15 15:34:03 +0100

    try with our own sh scripts

---
+++ travisci/get_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo 'Getting BioPerl'
+if [ ! -f bioperl-release-1-6-924.zip ]; then
+  wget https://github.com/bioperl/bioperl-live/archive/bioperl-release-1-6-924.zip
+  unzip -q bioperl-release-1-6-924.zip
+fi
+
+echo 'Getting HTSlib'
+if [ ! -d htslib ]; then
+  git clone --branch master --depth 1 https://github.com/samtools/htslib.git
+fi
+
+echo 'Getting Bio::DB::HTS'
+if [ ! -d Bio-HTS ]; then
+  git clone --branch master --depth 1 https://github.com/Ensembl/Bio-HTS.git
+fi
+
+echo 'Getting jksrc'
+if [ ! -f jksrc.zip ]; then
+  wget http://hgdownload.cse.ucsc.edu/admin/jksrc.zip
+  unzip -q jksrc.zip
+fi
