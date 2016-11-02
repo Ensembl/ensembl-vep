f5f9701e325159044734f5c6a08d8cab2a90e603
William McLaren <wm2@ebi.ac.uk>
2016-11-02 11:17:34 +0000

    fix precedence error again?

--- modules/Bio/EnsEMBL/VEP/Utils.pm
+++ modules/Bio/EnsEMBL/VEP/Utils.pm
@@ -64,7 +64,7 @@ use vars qw(@ISA @EXPORT_OK);
   &get_version_string
 );
 
-our ($CAN_USE_PERLIO_GZIP, $CAN_USE_GZIP, $CAN_USE_IO_UNCOMPRESS, $GunzipError);
+our ($CAN_USE_PERLIO_GZIP, $CAN_USE_GZIP, $CAN_USE_IO_UNCOMPRESS);
 
 BEGIN {
 
@@ -226,7 +226,8 @@ sub get_compressed_filehandle {
     return $fh;
   }
   if($CAN_USE_IO_UNCOMPRESS) {
-    return IO::Uncompress::Gunzip->new($file, MultiStream => $multi) or die("ERROR: $GunzipError");
+    my $fh = IO::Uncompress::Gunzip->new($file, MultiStream => $multi) or die("ERROR: $GunzipError");
+    return $fh;
   }
   elsif($CAN_USE_GZIP) {
     open my $fh, "gzip -dc $file |" or die("ERROR: $!");
