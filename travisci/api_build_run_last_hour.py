7691ee16f6e2e9e18241e0d8907491b5030bfc6c
Alessandro Vullo <avullo@ebi.ac.uk>
2017-10-23 14:45:45 +0100

    [ENSCORESW-2343]. Bug as we want the total number of seconds.

--- travisci/api_build_run_last_hour.py
+++ travisci/api_build_run_last_hour.py
@@ -7,7 +7,7 @@ def api_build_run_last_hour(x):
     return x['state'] != 'canceled' and \
                          x['event_type'] == 'api' and \
                          (x['finished_at'] == None or \
-                          float((datetime.fromtimestamp(time.time()) - datetime.strptime(x['finished_at'],'%Y-%m-%dT%H:%M:%SZ')).seconds)/3600 < 1.0)
+                          float((datetime.fromtimestamp(time.time()) - datetime.strptime(x['finished_at'],'%Y-%m-%dT%H:%M:%SZ')).total_seconds())/3600 < 1.0)
 
 builds = list(filter(api_build_run_last_hour, json.load(sys.stdin)['builds']))
 print(len(builds) > 0)
