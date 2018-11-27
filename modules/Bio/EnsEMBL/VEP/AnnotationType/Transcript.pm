e570f5cc854d9b905a00488a456ff80d8245601f
aparton <aparton@ebi.ac.uk>
2018-11-27 09:57:48 +0000

    Updated xref dumping code

--- modules/Bio/EnsEMBL/VEP/AnnotationType/Transcript.pm
+++ modules/Bio/EnsEMBL/VEP/AnnotationType/Transcript.pm
@@ -211,14 +211,18 @@ sub filter_transcript {
       # or 4540 for MT transcripts
       (
         $self->{source_type} eq 'refseq' &&
-        ($t->stable_id || '') !~ /^[A-Z]{2}\_\d+|^\d{4}$/
-      ) ||
+        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/ || 
+        (defined($t->display_xref) && ($t->display_xref->{display_id} || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/))
+      ) 
+      
+      ||
 
       # and the same from the merged cache
       (
         $self->{source_type} eq 'merged' &&
         ($t->{_source_cache} || '') eq 'RefSeq' &&
-        ($t->stable_id || '') !~ /^[A-Z]{2}\_\d+|^\d{4}$/
+        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/ || 
+        (defined($t->display_xref) && ($t->display_xref->{display_id} || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/))
       )
     )
   ) {
