4b21f539ddea77cede2e07d9508607a9cf348acd
Helen Schuilenburg <25033562+helensch@users.noreply.github.com>
2020-02-26 10:40:23 +0000

    Yearly copyright update (#700)

--- modules/Bio/EnsEMBL/VEP/OutputFactory/Tab.pm
+++ modules/Bio/EnsEMBL/VEP/OutputFactory/Tab.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2016-2019] EMBL-European Bioinformatics Institute
+Copyright [2016-2020] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
