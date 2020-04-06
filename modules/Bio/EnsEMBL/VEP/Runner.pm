b9a60211daf059263b6d3d68f5ce114cf4b27740
aparton <aparton@ebi.ac.uk>
2020-04-06 17:27:06 +0100

    Turn on shift_3prime when shift_genomic is switched on

--- modules/Bio/EnsEMBL/VEP/Runner.pm
+++ modules/Bio/EnsEMBL/VEP/Runner.pm
@@ -766,6 +766,13 @@ sub post_setup_checks {
     $self->status_msg("INFO: --shift_hgvs has been set to zero, setting shift_3prime to 0\n");
     $self->param('shift_3prime', 0);
   }
+  
+  if(defined($self->param('shift_genomic')) && $self->param('shift_genomic') eq 1 ) {
+    if(defined($self->param('shift_3prime')) && $self->param('shift_3prime') eq 0 ) {
+      $self->status_msg("INFO: --shift_genomic has been set to 1, setting shift_3prime to 1\n");
+    }
+    $self->param('shift_3prime', 1);
+  }
 
   return 1;
 }
