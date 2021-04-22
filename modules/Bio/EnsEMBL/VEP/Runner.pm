0c8589192b8b3df5ec13d40bf474f492a97756a7
at7 <anja@ebi.ac.uk>
2021-04-22 11:17:37 +0100

    fix condition for setting grch37 port

--- modules/Bio/EnsEMBL/VEP/Runner.pm
+++ modules/Bio/EnsEMBL/VEP/Runner.pm
@@ -114,7 +114,7 @@ sub init {
   # log start time
   $self->stats->start_time();
 
-  if(defined($self->param('assembly')) && lc($self->param('assembly')) eq 'grch37' && defined($self->param('database') && !defined($self->param('port')))) {
+  if(defined($self->param('assembly')) && lc($self->param('assembly')) eq 'grch37' && defined($self->param('database')) && !defined($self->param('port'))) {
     $self->param('port', 3337);
   }
 
