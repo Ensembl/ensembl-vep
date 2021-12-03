d25b835cb3037dd39a65db4e8eafb650c017f828
Likhitha Surapaneni <10923198+likhitha-surapaneni@users.noreply.github.com>
2021-12-03 15:37:22 +0000

    Minor fix

--- nextflow/setup-images.sh
+++ nextflow/setup-images.sh
@@ -1,3 +1,3 @@
-mkdir -p singularity-images1
-singularity pull --name singularity-images1/bcftools.sif docker://quay.io/biocontainers/bcftools:1.13--h3a49de5_0
-singularity pull --name singularity-images1/vep.sif docker://ensemblorg/ensembl-vep
+mkdir -p singularity-images
+singularity pull --name singularity-images/bcftools.sif docker://quay.io/biocontainers/bcftools:1.13--h3a49de5_0
+singularity pull --name singularity-images/vep.sif docker://ensemblorg/ensembl-vep
