8e8b39f141a13b6bcd35c9bd23e018046800bf81
jamie-m-a <jma@ebi.ac.uk>
2022-01-14 13:33:14 +0000

    2022 annual copyright update

--- convert_cache.pl
+++ convert_cache.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-# Copyright [2016-2021] EMBL-European Bioinformatics Institute
+# Copyright [2016-2022] EMBL-European Bioinformatics Institute
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
