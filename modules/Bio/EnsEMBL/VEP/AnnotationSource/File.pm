1e3e4c2b9b0e744a79e9bca3d10e82803e9f42bb
Likhitha Surapaneni <10923198+likhitha-surapaneni@users.noreply.github.com>
2022-06-17 09:47:24 +0100

    Check for overlap before pattern matching with COSMIC annotation

--- modules/Bio/EnsEMBL/VEP/AnnotationSource/File.pm
+++ modules/Bio/EnsEMBL/VEP/AnnotationSource/File.pm
@@ -345,9 +345,8 @@ sub annotate_VariationFeature {
   my $vf = shift;
 
   my $overlap_result = $self->_record_overlaps_VF($vf);
-
-  if(@{$self->_create_records($overlap_result)}[0]->{'name'} =~  /^COSV/) {
-    push @{$vf->{_custom_annotations}->{$self->short_name}}, @{$self->_create_records($overlap_result)} if $overlap_result && !(grep{$_->{'name'} eq @{$self->_create_records($overlap_result)}[0]->{'name'}} @{$vf->{_custom_annotations}->{$self->short_name}});
+  if ($overlap_result && @{$self->_create_records($overlap_result)}[0]->{'name'} =~  /^COSV/) {
+    push @{$vf->{_custom_annotations}->{$self->short_name}}, @{$self->_create_records($overlap_result)} if !(grep{$_->{'name'} eq @{$self->_create_records($overlap_result)}[0]->{'name'}} @{$vf->{_custom_annotations}->{$self->short_name}});
   }
   else {
     push @{$vf->{_custom_annotations}->{$self->short_name}}, @{$self->_create_records($overlap_result)} if $overlap_result;
