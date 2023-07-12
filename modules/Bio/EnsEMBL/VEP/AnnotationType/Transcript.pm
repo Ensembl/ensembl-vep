906ac0c6f4577b3d25098bd59a995ed79993a07d
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2023-07-12 15:32:01 +0100

    Allow non rna- prefix in transcript stable id

--- modules/Bio/EnsEMBL/VEP/AnnotationType/Transcript.pm
+++ modules/Bio/EnsEMBL/VEP/AnnotationType/Transcript.pm
@@ -210,10 +210,10 @@ sub filter_transcript {
     (
       # we only want RefSeq transcripts e.g. NM_12930,
       # or 4540 for MT transcripts
-      # or rna-TRNP for MT transcripts
+      # or TRNP (or, rna-TRNP for e105 cahce or before) for MT transcripts
       (
         $self->{source_type} eq 'refseq' &&
-        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$|^rna-[A-Z]{3,4}\d{0,1}/ || 
+        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$|^(rna-)?[A-Z]{3,4}\d{0,1}/ || 
         (defined($t->display_xref) && ($t->display_xref->{display_id} || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/))
       ) 
       
@@ -223,7 +223,7 @@ sub filter_transcript {
       (
         $self->{source_type} eq 'merged' &&
         ($t->{_source_cache} || '') eq 'RefSeq' &&
-        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$|^rna-[A-Z]{3,4}\d{0,1}/ || 
+        !(($t->stable_id || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$|^(rna-)?[A-Z]{3,4}\d{0,1}/ || 
         (defined($t->display_xref) && ($t->display_xref->{display_id} || '') =~ /^[A-Z]{2}\_\d+|^\d{4}$/))
       )
     )
