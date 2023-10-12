2ed61e4a35061898b080d479efabdc0e9116ac95
Dan Bolser <dan.bolser@outsee.co.uk>
2023-10-12 09:39:34 +0100

    ignore blank lines in the config.ini
    currently these generate ugly warnings. I use space and comments in the config file for readability

--- modules/Bio/EnsEMBL/VEP/Config.pm
+++ modules/Bio/EnsEMBL/VEP/Config.pm
@@ -1018,6 +1018,7 @@ sub read_config_from_file {
 
   while(<CONFIG>) {
     next if /^\#/;
+    next if /^\s*$/;
 
     # preserve spaces between quotes
     s/\s+(?=(?:(?:[^"]*"){2})*[^"]*"[^"]*$)/___SPACE___/g;
