49e8831e3ef52a57f5bb066a132b7e9e5c504a3e
Likhitha Surapaneni <10923198+likhitha-surapaneni@users.noreply.github.com>
2023-10-31 16:01:35 +0000

    Deterministic order for software versions

--- modules/Bio/EnsEMBL/VEP/OutputFactory/BaseTab.pm
+++ modules/Bio/EnsEMBL/VEP/OutputFactory/BaseTab.pm
@@ -96,7 +96,7 @@ sub headers {
         (defined($software_version_data->{$_}->{sub}) ? '.'.substr($software_version_data->{$_}->{sub}, 0, 7) : '')
       )
     }
-    grep {$_ ne 'ensembl-vep'} keys %{$software_version_data};
+    grep {$_ ne 'ensembl-vep'} sort keys %{$software_version_data};
 
   push @headers, "## $_ version ".$info->{version_data}->{$_} for keys %{$info->{version_data} || {}};
 
