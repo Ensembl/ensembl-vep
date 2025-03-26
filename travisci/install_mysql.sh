6fb1ec4132cabf6b4fca6f7d21983c46790cfd15
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2025-03-26 22:47:26 +0000

    Use super-user privilege for apt install too

--- travisci/install_mysql.sh
+++ travisci/install_mysql.sh
@@ -12,7 +12,7 @@ sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-p
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 # apt-get update
-apt-get install -y mysql-server-5.7 mysql-client-5.7 mysql-client-core-5.7
+sudo apt-get install -y mysql-server-5.7 mysql-client-5.7 mysql-client-core-5.7
 mysql -e 'SET GLOBAL local_infile=1;'
 
 rm mysql-apt-config_0.8.12-1_all.deb
