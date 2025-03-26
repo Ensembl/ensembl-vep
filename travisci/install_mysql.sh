7460eba1fa0f1a3c8712f7e1afe2b0f5c9d29fd2
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2025-03-26 22:36:58 +0000

    Use super-user privilege

--- travisci/install_mysql.sh
+++ travisci/install_mysql.sh
@@ -1,16 +1,16 @@
 export DEBIAN_FRONTEND=noninteractive
 
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select focal'
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select focal'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
 
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
-dpkg -i mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 # apt-get update
 apt-get install -y mysql-server-5.7 mysql-client-5.7 mysql-client-core-5.7
 mysql -e 'SET GLOBAL local_infile=1;'
