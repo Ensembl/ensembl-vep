d2c34b5e038cfc22824e73d5e5d3f4902f6af48c
jamie-m-a <jma@ebi.ac.uk>
2025-07-03 12:14:25 +0100

    patches

--- t/test-genome-DBs/homo_vepiens/otherfeatures/table.sql
+++ t/test-genome-DBs/homo_vepiens/otherfeatures/table.sql
@@ -490,7 +490,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`),
   KEY `species_value_idx` (`species_id`,`meta_value`)
-) ENGINE=MyISAM AUTO_INCREMENT=896 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=897 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `meta_coord` (
   `table_name` varchar(40) NOT NULL,
